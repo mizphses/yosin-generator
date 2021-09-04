9c7235946ef277df98a066d01d73e5332a5255f9
mizphses <fuminori@mizphses.com>
2021-09-04 10:15:36 +0900

    Vaccination Type

--- app.py
+++ app.py
@@ -46,9 +46,10 @@ def genpdf():
   ruby=request.form.get('ruby')
   phone=request.form.get('phone')
   birthdate=request.form.get('birthdate')
+  vaccination_date=datetime.strptime(request.form.get('vaccination_date'), '%Y-%m-%d')
   birth = datetime.strptime(birthdate, '%Y-%m-%d')
-  age = str(relativedelta(today, birth).years)
-  sex=request.form.get('sex')
+  age = str(relativedelta(vaccination_date, birth).years)
+  sex=checkExistTf(request.form.get('sex'))
   q1=request.form.get('q1')
   q2=request.form.get('q2')
   q3=request.form.get('q3')
@@ -93,8 +94,10 @@ def genpdf():
   q5toy = checkExistTf(request.form.get('touyaku'))
   sharepdf = checkExistTf(request.form.get('pdf'))
   maker = checkExistTf(request.form.get('maker'))
-  if maker == 'true':
+  if maker == 'astrazeneca' or maker == 'dontknow':
     q5m = checkExistTf(request.form.get('q5-addition-mousai'))
+  else:
+    q5m = "false"
   if q1 == 'false':
     firstvac = (datetime.strptime(request.form.get('firstvaccine'), '%Y-%m-%d'))
   else:
@@ -107,18 +110,15 @@ def genpdf():
     day = "0"+str(birth.day)
   else :
     day = str(birth.day)
-  if maker == 'true':
-    json_file = {"address":address, "name":name, "ruby":ruby, "phone":phone,"year":str(birth.year), "month":month, "day":day, "age": age, "sex": sex, "q1":[q1, str(firstvac.month), str(firstvac.day)], "q2":q2, "q3":q3, "q4":[q4, med, ov65, ov60, eld, underlying, underlying_sikkan], "q5":[q5,q5h,q5k,q5l,q5b,q5n,q5i,q5m,q5otc,q5chiryou,q5sara,q5mb,q5otm,q5toy], "q6":[q6, sick], "q7":[q7, guai], "q8":q8, "q9":[q9, anaphylaxie_item], "q10":[q10, vaccine_item, vaccine_shoujou], "q11":q11, "q12":[q12, vaccinated_type, vaccinated_when], "q13":q13}
-  else:
-    json_file = {"address":address, "name":name, "ruby":ruby, "phone":phone,"year":str(birth.year), "month":month, "day":day, "age": age, "sex": sex, "q1":[q1, str(firstvac.month), str(firstvac.day)], "q2":q2, "q3":q3, "q4":[q4, med, ov65, ov60, eld, underlying, underlying_sikkan], "q5":[q5,q5h,q5k,q5l,q5b,q5n,q5i,q5otc,q5chiryou,q5sara,q5mb,q5otm,q5toy], "q6":[q6, sick], "q7":[q7, guai], "q8":q8, "q9":[q9, anaphylaxie_item], "q10":[q10, vaccine_item, vaccine_shoujou], "q11":q11, "q12":[q12, vaccinated_type, vaccinated_when], "q13":q13}
+  json_file = {"address":address, "name":name, "ruby":ruby, "phone":phone,"year":str(birth.year), "month":month, "day":day, "age": age, "sex": sex, "maker": maker, "q1":[q1, str(firstvac.month), str(firstvac.day)], "q2":q2, "q3":q3, "q4":[q4, med, ov65, ov60, eld, underlying, underlying_sikkan], "q5":[q5,q5h,q5k,q5l,q5b,q5n,q5i,q5m,q5otc,q5chiryou,q5sara,q5mb,q5otm,q5toy], "q6":[q6, sick], "q7":[q7, guai], "q8":q8, "q9":[q9, anaphylaxie_item], "q10":[q10, vaccine_item, vaccine_shoujou], "q11":q11, "q12":[q12, vaccinated_type, vaccinated_when], "q13":q13}
   if sharepdf == 'true':
     fontname_g = "ipag-mona"
     default_font_size = 12
     font_path = "./resource/ipag-mona.ttf"
-    if maker == 'false':
-      file_in = "./resource/vaccine_yoshin_pfizer_takeda.pdf"
-    else:
+    if maker == 'astrazeneca' or maker == 'dontknow':
       file_in = "./resource/vaccine_yoshin_astra.pdf"
+    else:
+      file_in = "./resource/vaccine_yoshin_pfizer_takeda.pdf"
     output = BytesIO()
     cc = canvas.Canvas(output)
     def write_yes_no(x,y,answer):
@@ -163,17 +163,7 @@ def genpdf():
             write_checkmark(228,497,answer[4])
             write_checkmark(284,497,answer[5])
             write_checkmark(384,497,answer[6])
-            if q5m is None:
-              if answer[7] == 'true':
-                write_checkmark(86,480,answer[7])
-                cc.drawString(130,480,answer[8])
-              write_checkmark(86,463,answer[9])
-              if answer[9] == 'true':
-                cc.drawString(190,463,answer[10])
-              write_checkmark(286,463,answer[11])
-              if answer[11] == 'true':
-                cc.drawString(330,463,answer[12])
-            else:
+            if maker == 'astrazeneca' or maker == 'dontknow':
               write_checkmark(85,480,answer[7])
               write_checkmark(192,480,answer[8])
               if answer[8] == 'true':
@@ -184,6 +174,16 @@ def genpdf():
             write_checkmark(286,463,answer[12])
             if answer[12] == 'true':
               cc.drawString(330,463,answer[13])
+            else:
+              if answer[8] == 'true':
+                write_checkmark(86,480,answer[8])
+                cc.drawString(130,480,answer[9])
+              write_checkmark(86,463,answer[10])
+              if answer[10] == 'true':
+                cc.drawString(190,463,answer[11])
+              write_checkmark(286,463,answer[12])
+              if answer[12] == 'true':
+                cc.drawString(330,463,answer[13])
         write_yes_no(442,488,answer[0])
 
 
